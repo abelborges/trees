02a5047551c0367e0e89f45fba02dc6f90b7547b
abelborges <brgs.abel@gmail.com>
2020-08-26 00:33:21 -0300

    allows numeric features

--- expected_entropy.R
+++ expected_entropy.R
@@ -1,17 +1,40 @@
 
-expected_entropy = function(given_numeric_feature) {
-  xx = x[[given_numeric_feature]]
-  thresholds = quantile(xx, seq(.01, .99, by = .01))
-  potential_entropies = sapply(thresholds, function(threshold) {
-    i = which(xx > threshold)
-    e1 = entropy(table(y[ i ])/n, k)
-    e2 = entropy(table(y[-i ])/n, k)
-    p = length(i)/n
-    p*e1 + (1-p)*e2
-  })
-  best_cut = which.min(entropies)
-  list(
-    best_cut = qs[best_cut],
-    entropy = potential_entropies[best_cut]
-  )
+is_ordinal = function(x) {
+  not_ordinal = is.character(x) | (is.factor(x) & !is.ordered(x))
+  !not_ordinal
+}
+
+function(of, given, n, k) {
+  if (!is_ordinal(given)) {
+    distribution = table(given)/n
+    cond_entropies = sapply(names(distribution), function(val) {
+      entropy(table(of[given == val])/n, k)
+    })
+    
+    list(
+      cut = NULL,
+      entropy = sum(distribution * cond_entropies)
+    )
+  } else {
+    ps = seq(.01, .99, by = .01)
+    qtype = if (is.factor(given)) 3 else 7 # see 'Types' in ?quantile
+    cuts = quantile(given, probs = ps, names = FALSE, type = qtype)
+    
+    entropy_by_cut = sapply(seq_along(cuts), function(i) {
+      idxs = which(given > cuts[i])
+      e1 = entropy(table(of[ idxs ])/n, k = 2)
+      e2 = entropy(table(of[-idxs ])/n, k = 2)
+      p = 1 - ps[i] # approximately length(idxs)/n
+      p*e1 + (1-p)*e2
+    })
+    
+    # which.min returns first match
+    # which.min(sample(entropy_by_cut)) would randomize in case there's a tie
+    best = which.min(entropy_by_cut)
+    
+    list(
+      cut = cuts[best],
+      entropy = entropy_by_cut[best]
+    )
+  }
 }
