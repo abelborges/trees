adb2eb2c0ea49f7d6c1e588bcd8f0879f19f19ef
abelborges <brgs.abel@gmail.com>
2020-08-29 00:11:55 -0300

    make all features ordinal

--- expected_entropy.R
+++ expected_entropy.R
@@ -1,48 +1,33 @@
-# TODO: use only binary splits, see ESLII section 9.2.4, page 310
 
 is_ordinal = function(x) {
-  not_ordinal = is.character(x) | (is.factor(x) & !is.ordered(x))
-  !not_ordinal
-}
-
-expected_entropy_categoric = function(of_y, given_x, n, k) {
-  distribution = table(given_x) / n
-  cond_entropies = sapply(names(distribution), function(val) {
-    entropy(table(of_y[given_x == val])/n, k)
-  })
-  
-  list(
-    cut = NULL,
-    entropy = sum(distribution * cond_entropies)
-  )
-}
-
-expected_entropy_numeric = function(of_y, given_x, n, k) {
-  ps = seq(.01, .99, by = .01)
-  qtype = if (is.factor(given_x)) 3 else 7 # see 'Types' in ?quantile
-  cuts = quantile(given_x, probs = ps, names = FALSE, type = qtype)
-  
-  entropy_by_cut = sapply(seq_along(cuts), function(i) {
-    idxs = which(given_x > cuts[i])
-    e1 = entropy(table(of_y[ idxs ])/n, k = 2)
-    e2 = entropy(table(of_y[-idxs ])/n, k = 2)
-    p = 1 - ps[i] # approximately length(idxs)/n
-    p*e1 + (1-p)*e2
-  })
-  
-  # which.min returns first match
-  # which.min(sample(entropy_by_cut)) would randomize in case there's a tie
-  best = which.min(entropy_by_cut)
-  
-  list(
-    cut = cuts[best],
-    entropy = entropy_by_cut[best]
-  )
+  is_not = is.character(x) | (is.factor(x) & !is.ordered(x))
+  !is_not
 }
 
 function(of_y, given_x, n, k) {
-  if (!is_ordinal(given_x))
-    expected_entropy_categoric(of_y, given_x, n, k)
-  else
-    expected_entropy_numeric(of_y, given_x, n, k)
+  xlevels = unique(given_x)
+  ordinal = is_ordinal(given_x)
+  
+  if (!ordinal) {
+    probs_class1 = sapply(xlevels, function(x) mean(of_y[given_x == x]))
+    xlevels = xlevels[order(probs_class1)]
+    given_x = factor(given_x, levels = xlevels, ordered = TRUE)
+  } else {
+    xlevels = sort(xlevels)
+  }
+  
+  entropy_by_cut = sapply(xlevels[-length(xlevels)], function(x) {
+    x_given = which(given_x > x)
+    e1 = entropy(table(of_y[ x_given ])/n, k)
+    e2 = entropy(table(of_y[-x_given ])/n, k)
+    p = length(x_given) / n
+    p*e1 + (1-p)*e2
+  })
+  
+  best = which.min(entropy_by_cut)
+  
+  list(
+    cut = if (ordinal) xlevels[best] else xlevels[seq_along(xlevels) > best],
+    entropy = entropy_by_cut[best]
+  )
 }
